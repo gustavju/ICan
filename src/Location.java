4e52cb895d4a5adc8a2a9d81164acb45366ca8a9
Hojjat Seyed <hojjatseyed@gmail.com>
2018-12-07 14:03:25 +0100

    Added Location

--- src/Location.java
+++ src/Location.java
@@ -1,2 +1,23 @@
 public class Location {
+
+    private double longitude;
+    private double latitude;
+
+    public Location(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void updateLocation(double longitude, double latitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
