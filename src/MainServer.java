2c84e1e084d9593aba8f399a557bb881c1b7286d
Stuix <abomdonoman@gmail.com>
2018-12-07 14:34:23 +0100

    MainServer påbörjad

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,2 +1,27 @@
 public class MainServer {
+
+    private String hostName;
+    private String serverName;
+    private String password;
+
+
+    public MainServer(String host, String server, String pass){
+        hostName=host;
+        server=serverName;
+        password=pass;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
