2ebc01519bf58a7406bd9a83b6870b6091a9f6e4
Stuix <abomdonoman@gmail.com>
2018-12-12 14:41:05 +0100

    GarbageTruckCallback, takeAction method

--- src/GarbageTruckCallback.java
+++ src/GarbageTruckCallback.java
@@ -18,14 +18,69 @@ public class GarbageTruckCallback implements MqttCallback {
     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
         String message = new String(mqttMessage.getPayload());
         System.out.println("Message received:\n\t" + message);
-        takeAction(message);
+        takeAction(topic, message);
 
     }
 
-    private void takeAction(String message) {
-        switch (message) {
+    private void takeAction(String topic, String message) {
+        switch (topic) {
+            case "trashcan1":
+                switchTrashcan1(message);
+                break;
+
+            case "trashcan2" :
+                switchTrashcan1(message);
+                break;
+
+            case "trashcan3" :
+                switchTrashcan1(message);
+                break;
+
+            case "server" :
+                switchServer(message);
+                break;
+
         }
     }
+
+    private void switchTrashcan1(String message){
+        switch(message.split(":")[0]){
+            case "temperature":
+                break;
+            case "trashlevel" :
+                break;
+        }
+    }
+
+    private void switchTrashcan2(String message){
+        switch(message.split(":")[0]){
+            case "temperature":
+                break;
+            case "trashlevel" :
+                break;
+        }
+    }
+
+    private void switchTrashcan3(String message){
+        switch(message.split(":")[0]){
+            case "temperature":
+                break;
+            case "trashlevel" :
+                break;
+        }
+    }
+
+    private void switchServer(String message){
+        switch(message.split(":")[0]){
+            case "temperature":
+                break;
+            case "trashlevel" :
+                break;
+        }
+    }
+
+
+
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
--- src/GarbageTruckMain.java
+++ src/GarbageTruckMain.java
@@ -1,8 +1,25 @@
 public class GarbageTruckMain {
 
-    public static void main(String [] args){
-      GarbageTruck truck1 =  new GarbageTruck(100,new Location(66.555,44.666));
+    private void run(String clientId, double longitude, double latitude) {
+        GarbageTruck garbageTruck = new GarbageTruck(100, new Location(longitude, latitude));
+        String[] subs = {clientId, "client2", "client3", "server"};
+        MQTTClient mqttClient = new MQTTClient("GarbageTruck", new GarbageTruckCallback(garbageTruck), subs);
 
+    }
+    public static void main(String [] args){
+        String clientId;
+        double longitude;
+        double latitude;
+        if (args.length == 0) {
+            clientId = "client1";
+            longitude = 66.00;
+            latitude = 66.00;
+        } else {
+            clientId = args[0];
+            longitude = Double.parseDouble(args[1]);
+            latitude = Double.parseDouble(args[2]);
+        }
+        new GarbageTruckMain().run(clientId, longitude, latitude);
 
     }
 }
