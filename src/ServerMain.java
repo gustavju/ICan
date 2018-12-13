1fe1d6dacdd2c4b6ff18314d9906a9808393b205
gustavjunedahl <gustav.junedahl@gmail.com>
2018-12-13 13:57:19 +0100

    Callback changes

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,8 +1,11 @@
+import org.json.JSONObject;
+
 public class ServerMain {
 
     private void run() {
-        MainServer server = new MainServer("", "", "");
-        server.mqttClient.sendMessage("discovery", "");
+        MainServer server = new MainServer();
+        server.mqttClient.sendMessage("trashcanDiscovery", "");
+        server.mqttClient.sendMessage("garbageTruckDiscovery", "");
     }
 
     public static void main(String[] args) {
