a3a5dce52fa24b38a196b8cc6f61e2a1b689a9dc
Hojjat Seyed <hojjatseyed@gmail.com>
2018-12-17 14:01:31 +0100

    toJson added for garbagetruck

--- src/GarbageTruck.java
+++ src/GarbageTruck.java
@@ -6,34 +6,34 @@ import java.util.UUID;
 
 public class GarbageTruck {
 
-    private final double MAX_CAPACITY= 100;
+    private final double MAX_CAPACITY = 100;
     private List<String> route;
     private Location location;
     private double capacity;
     protected MQTTClient mqttClient;
     private String garbageTruckId;
 
-    public GarbageTruck(Location location){
+    public GarbageTruck(Location location) {
         garbageTruckId = UUID.randomUUID().toString();
         this.location = location;
         capacity = 0;
-        String[] subs ={garbageTruckId,"garbagetruckDiscovery"};
+        String[] subs = {garbageTruckId, "garbagetruckDiscovery"};
         mqttClient = new MQTTClient(garbageTruckId, new GarbageTruckCallback(this), subs);
-        mqttClient.sendMessage("garbagetruckDiscovery",this.toString());
+        mqttClient.sendMessage("garbagetruckDiscovery", this.toString());
         route = new ArrayList<String>();
     }
 
-    public void setLocation(Location location){
-        this.location=location;
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
-    public void addTrashcan(String trashcanID){
+    public void addTrashcan(String trashcanID) {
         route.add(trashcanID);
     }
 
-    public void setRoute(Trashcan trashcan){
+    public void setRoute(Trashcan trashcan) {
 
-        if((capacity+trashcan.getLevel())>=MAX_CAPACITY*0.8){
+        if ((capacity + trashcan.getLevel()) >= MAX_CAPACITY * 0.8) {
             System.out.println("Error: Route is full");
             return;
         }
@@ -42,38 +42,44 @@ public class GarbageTruck {
         trashcan.changeStatus(CanStatus.PICKUPPENDING);
     }
 
-    public void fillTruck(Trashcan trashcan){
-        if((capacity + trashcan.getLevel()/10)> MAX_CAPACITY){
+    public void fillTruck(Trashcan trashcan) {
+        if ((capacity + trashcan.getLevel() / 10) > MAX_CAPACITY) {
             System.out.println("Error: Garbage Truck is Full");
         }
 
-        if((capacity + trashcan.getLevel()/10)> MAX_CAPACITY*0.9){
+        if ((capacity + trashcan.getLevel() / 10) > MAX_CAPACITY * 0.9) {
             System.out.println("Warning: Garbage Truck almost Full");
-            capacity += trashcan.getLevel()/10;
-        }else{
-            capacity += trashcan.getLevel()/10;
+            capacity += trashcan.getLevel() / 10;
+        } else {
+            capacity += trashcan.getLevel() / 10;
         }
 
     }
 
-    public String getGarbageTruckId(){
+    public String getGarbageTruckId() {
         return garbageTruckId;
     }
 
+    public String toJson() {
+        return
+                "{\"garbageTruckId\":" + "\"" + garbageTruckId + "\"" +
+                        "\"location\":" + "\"" + location +
+                        "\"" + "\"capacity\":" + "\"" + capacity + "\"";
+    }
 
 
-    public List<String> getRoute(){
+    public List<String> getRoute() {
         return route;
     }
 
-    public void emptyTruck(){
+    public void emptyTruck() {
         route.clear();
     }
 
     @Override
     public String toString() {
         return "{" +
-            "garbageTruckId="+ garbageTruckId +
-            '}';
+                "garbageTruckId=" + garbageTruckId +
+                '}';
     }
 }
